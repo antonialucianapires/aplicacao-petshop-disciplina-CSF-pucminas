7e5a9fff07410fcfa69c7b38bde9489b882dfd91
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-10-15 14:42:48 -0300

    config: base do projeto

---
+++ petshop/src/test/java/com/antonialucianapires/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonialucianapires.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
